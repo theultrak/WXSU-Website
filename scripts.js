2fcf8fa13b3fd2777242793ded61c4e2d1249ea5
Jeremy Scheuerman <jerm.s222@gmail.com>
2022-05-04 18:36:24 -0400

    added meta tags cleaned up some code

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
+/*This site was developed for Salisbury's WXSU Student run radio club by Jeremy Scheuerman
+If you have questions about the source code of the site, or want to checkout my other work
+please visit my portfolio here to find my work and contact information
+https://jeremy-scheuerman-portfolio.helix5712.com/
+ */
 const hamburger = document.querySelector(".hamburger");
 const navLink = document.querySelector(".nav__link");
 hamburger.addEventListener("click", () => {
