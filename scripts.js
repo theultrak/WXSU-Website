764b46cf56c38f6c96377cc947272410e53d37ea
Jeremy Scheuerman <jerm.s222@gmail.com>
2022-05-04 22:46:30 -0400

    added slideshow, must fix slideshow responsinveness issues

--- scripts.js
+++ scripts.js
@@ -2,7 +2,15 @@
 If you have questions about the source code of the site, or want to checkout my other work
 please visit my portfolio here to find my work and contact information
 https://jeremy-scheuerman-portfolio.helix5712.com/
- */ let slideIndex = 0;
+ */
+
+const hamburger = document.querySelector(".hamburger");
+const navLink = document.querySelector(".nav__link");
+hamburger.addEventListener("click", () => {
+  navLink.classList.toggle("hide");
+});
+
+let slideIndex = 0;
 showSlides();
 
 function showSlides() {
@@ -16,10 +24,6 @@ function showSlides() {
     slideIndex = 1;
   }
   slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  setTimeout(showSlides, 3000);
+  // Change image every 2 seconds
 }
-const hamburger = document.querySelector(".hamburger");
-const navLink = document.querySelector(".nav__link");
-hamburger.addEventListener("click", () => {
-  navLink.classList.toggle("hide");
-});
